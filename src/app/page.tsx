8a64833788959a612e3e74524fcc1b5ce0fe95b3
gabrielfst30 <gabrielfst30@gmail.com>
2024-06-11 00:40:43 -0300

    feature: enviando props e icon para hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import { Hero } from "@/components/hero";
 import Submenu from "@/components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/home.type";
+import { Phone } from "lucide-react";
 
 export default async function Home() {
-  const data: HomeProps = await getDataHome();
+  const { object }: HomeProps = await getDataHome();
   //hack para ver objeto de dois níveis
   return (
     <main>
       <Submenu />
+      <Hero
+        heading={object.metadata.heading}
+        buttonTitle={object.metadata.cta_button.title}
+        buttonUrl={object.metadata.cta_button.url}
+        bannerUrl={object.metadata.banner.url}
+        icon={<Phone size={24} color="#FFF"/>}
+      />
     </main>
   );
 }
