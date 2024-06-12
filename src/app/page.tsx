9760110824496dfea4d9a2cad343466393117bdb
gabrielfst30 <gabrielfst30@gmail.com>
2024-06-12 00:45:50 -0300

    feature: importando alguns componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import { Container } from "@/components/container";
 import { Hero } from "@/components/hero";
+import { Services } from "@/components/home/services";
 import Submenu from "@/components/home/submenu";
 import { getDataHome } from "@/utils/actions/get-data";
 import { HomeProps } from "@/utils/home.type";
@@ -15,8 +17,11 @@ export default async function Home() {
         buttonTitle={object.metadata.cta_button.title}
         buttonUrl={object.metadata.cta_button.url}
         bannerUrl={object.metadata.banner.url}
-        icon={<Phone size={24} color="#FFF"/>}
+        icon={<Phone size={24} color="#FFF" />}
       />
+      <Container>
+        <Services object={object}/>
+      </Container>
     </main>
   );
 }
