7002de00147489c94c261885311f4685169f6afe
gabrielfst30 <gabrielfst30@gmail.com>
2024-07-01 15:30:09 -0300

    feature: chamando req para montar pagina dinamica

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,9 +1,15 @@
+import { getItemBySlug } from "@/utils/actions/get-data";
+import { PostProps } from "@/utils/post.type";
 import styles from "./styles.module.scss";
 
 //em rotas dinamicas eu recebo o slug pelo parametro e é necessário tipagem
-export default function Page({ params: { slug }}: {
-    params: { slug: string }
+export default async function Page({
+  params: { slug },
+}: {
+  params: { slug: string };
 }) {
+  const { object }: PostProps = await getItemBySlug(slug);
+
   return (
     <div>
       <h1>Página {slug}</h1>
